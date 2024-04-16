30aa2a9948f75e86ed27b12f598765e294365af0
weilinfox <caiweilin@iscas.ac.cn>
2024-04-16 17:32:29 +0800

    Add ruyismoke test ruyi_smoke_device_provision
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

---
+++ testcases/smoke-test/ruyismoke/ruyi_smoke_device_provision/common/device_lib.sh
@@ -0,0 +1,146 @@
+#!/usr/bin/bash
+
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more detaitest -f.
+
+# #############################################
+# @Author       :   KotorinMinami
+# @Contributor  :   weilinfox
+# @Contact      :   huangshuo4@gmail.com
+# @Date         :   2024/1/29
+# @License      :   Mulan PSL v2
+# @Desc         :   ruyisdk mugen device libs
+# #############################################
+
+source "${OET_PATH}"/libs/locallibs/common_lib.sh
+source "../common/common_lib.sh"
+
+result_item=()
+
+function test_ouput() {
+    local output
+    output=$(grep "$2" $1 | awk '{print $2}' | tail -1)
+    if [[ "$output" == '(y/N)' ]]; then
+        result_item=('y' 'n');
+    elif [[ "$output" =~ (1-.) ]]; then
+        result_item=($(seq ${output:0-2:1}));
+    else
+        result_item=('e');
+    fi
+}
+
+function recursion_run() {
+    local now_exec=$1
+    local end_exec=$2
+
+    if [[ ${#now_exec} -gt 100 ]]; then
+        LOG_ERROR "Quit test due to $now_exec longer then 100"
+        return 1
+    fi
+
+    if [[ "$end_exec" == "y" ]]; then
+        echo -e "\nHappy hacking! y y" >> /tmp/ruyi_device/output
+    elif [ ! -z "$end_exec" ] && [ "$end_exec" != "0" ]; then
+        local ret
+        echo -e $now_exec | ruyi device provision 2>&1 > /tmp/ruyi_device/output
+        ret=$?
+        echo -e "\nHappy hacking! $(expr $end_exec - 1) $ret" >> /tmp/ruyi_device/output
+    else
+        echo -e $now_exec | ruyi device provision 2>&1 > /tmp/ruyi_device/output
+    fi
+
+    grep 'Happy hacking!' /tmp/ruyi_device/output
+    if [[ $? -eq 0 ]]; then
+        local now_exec_f
+        now_exec_f=$(echo -E "$now_exec" | sed 's/\\n//g')
+        now_exec_f=$(echo -E "$now_exec_f" | sed 's$/$_$g')
+        mv /tmp/ruyi_device/output /tmp/ruyi_device/output_${now_exec_f}
+        rm -rf "$(get_ruyi_dir)"/distfiles/*
+        rm -rf "$(get_ruyi_data_dir)"/blobs/*
+        return 0;
+    fi
+
+    local ret
+    ret=0
+    grep "Proceed with flashing" /tmp/ruyi_device/output
+    if [[ $? -eq 0 ]]; then
+        rm -rf /tmp/ruyi_device/test
+        touch /tmp/ruyi_device/test
+        recursion_run "$now_exec\nn" 2
+        ret=$(expr $ret + $?)
+        # stop and quit
+        #recursion_run "$now_exec\ny" 2
+        #ret=$(expr $ret + $?)
+        return $ret;
+    fi
+    grep "Please give the path for the target's whole disk" /tmp/ruyi_device/output
+    if [[ $? -eq 0 ]]; then
+        #rm -rf /tmp/ruyi_device/test
+        #touch /tmp/ruyi_device/test
+        #recursion_run "$now_exec\n/tmp/ruyi_device/test" 1
+        recursion_run "$now_exec" "y"
+        ret=$(expr $ret + $?)
+        return $ret;
+        return 0
+    fi
+    grep 'Proceed' /tmp/ruyi_device/output
+    if [[ $? -eq 0 ]]; then
+        test_ouput /tmp/ruyi_device/output 'Proceed'
+        local next_step=(${result_item[@]})
+        for step in ${next_step[@]}; do
+            [ $step = 'n' ] && recursion_run "$now_exec\n$step" "2"
+            [ $step = 'y' ] && recursion_run "$now_exec\n$step"
+            # [[ $stop =~ [0-9]+ ]] && recursion_run "$now_exec\n$step"
+        done
+        return $ret;
+    fi
+    grep 'Choice' /tmp/ruyi_device/output
+    if [[ $? -eq 0 ]]; then
+        test_ouput /tmp/ruyi_device/output 'Choice'
+        local next_step=(${result_item[@]})
+        for step in ${next_step[@]}; do
+            end_exec=
+            [ $step = 'n' ] && end_exec=2
+            recursion_run "$now_exec\n$step" $end_exec
+            ret=$(expr $ret + $?)
+        done
+        return $ret;
+    fi
+    grep 'Continue' /tmp/ruyi_device/output
+    if [[ $? -eq 0 ]]; then
+        test_ouput /tmp/ruyi_device/output 'Continue'
+        local next_step=(${result_item[@]})
+        for step in ${next_step[@]}; do
+            end_exec=
+            [ $step = 'n' ] && end_exec=2
+            recursion_run "$step" $end_exec
+            ret=$(expr $ret + $?)
+        done
+        return $ret;
+    fi
+    mv /tmp/ruyi_device/output /tmp/ruyi_device/output_${now_exec}
+
+    return 1;
+}
+
+function test_res() {
+    local file=$1
+    local res=0
+    local ret=0
+
+    ret=$(grep 'Happy hacking!' $file)
+    res=$(expr $res + $?)
+
+    local ret_e=$(echo $ret | awk '{print $3}')
+    local ret_g=$(echo $ret | awk '{print $4}')
+
+    ( [ $ret_e = 0 ] && [ $ret_g = 0 ] ) || ( [ $ret_e != 0 ] && [ $ret_g != 0 ] ) || ( [ $ret_e = y ] && [ $ret_g = y ] )
+    res=$(expr $res + $?)
+
+    return $res
+}
---
+++ testcases/smoke-test/ruyismoke/ruyi_smoke_device_provision/ruyi_smoke_device_provision.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/bash
+
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more detaitest -f.
+
+# #############################################
+# @Author       :   KotorinMinami
+# @Contributor  :   weilinfox
+# @Contact      :   huangshuo4@gmail.com
+# @Date         :   2023/11/28
+# @License      :   Mulan PSL v2
+# @Desc         :   ruyisdk device smoke test
+# #############################################
+
+source "./common/device_lib.sh"
+source "$OET_PATH/testcases/cli-test/ruyi/common/common_lib.sh"
+
+EXECUTE_T=120m
+
+function pre_test() {
+    LOG_INFO "Start environmental preparation."
+    install_ruyi || LOG_ERROR "Install ruyi error"
+    mkdir /tmp/ruyi_device
+    LOG_INFO "End of environmental preparation!"
+}
+
+function run_test() {
+    LOG_INFO "Start to run test."
+
+    ruyi update
+
+    recursion_run ''
+    CHECK_RESULT $? 0 0 "Check recursion run failed"
+
+    ls_output=($(ls /tmp/ruyi_device/output*))
+    for file in ${ls_output[@]}
+    do
+        test_res $file
+        CHECK_RESULT $? 0 0 "Check file $file failed"
+    done
+
+    rm -rf /tmp/ruyi_device/test
+    tar zcf "${OET_PATH}"/logs/ruyismoke/ruyi_smoke_device_provision/$(date +'%Y-%m-%d-%H:%M:%S').tar.gz /tmp/ruyi_device
+    CHECK_RESULT $? 0 0 "Backup temp log file $file failed"
+
+    LOG_INFO "End of the test."
+}
+
+function post_test() {
+    LOG_INFO "start environment cleanup."
+    rm -rf /tmp/ruyi_device
+    remove_ruyi
+    LOG_INFO "Finish environment cleanup!"
+}
+
+main "$@"
