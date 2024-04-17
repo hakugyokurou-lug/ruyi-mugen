876d91cfe021fe5670c7f3edfb0f7256a496cb0c
weilinfox <caiweilin@iscas.ac.cn>
2024-04-17 15:41:06 +0800

    Not run getuto automatically
    Signed-off-by: weilinfox <caiweilin@iscas.ac.cn>

--- dep_install.sh
+++ dep_install.sh
@@ -28,7 +28,8 @@ common_dep(){
     sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y sudo expect psmisc iputils-ping make python3-paramiko python3-six || echo Not apt distro
     sudo dnf upgrade -y && sudo dnf install sudo expect psmisc make iputils python3-six python3-paramiko -y || echo Not rpm distro
     sudo pacman --noconfirm -Syuu && sudo pacman --need --noconfirm -S sudo expect psmisc make iputils python-six python-paramiko || echo Not archlinux distro
-    sudo emerge-webrsync && sudo getuto && sudo emerge --color=n --getbinpkg --noreplace --autounmask=y app-admin/sudo dev-tcltk/expect sys-process/psmisc dev-build/make dev-python/paramiko dev-python/six || echo Not gentoo linux
+    # if /etc/portage/gnupg/pubring.kbx not appears, run ``getuto`` manually
+    sudo emerge-webrsync && sudo emerge --color=n --getbinpkg --noreplace --autounmask=y app-admin/sudo dev-tcltk/expect sys-process/psmisc dev-build/make dev-python/paramiko dev-python/six || echo Not gentoo linux
 }
 
 jenkins_dep() {
